3aa6efa3295cb8362f6e32fdace41b57915c845b
Mau Munguia <maumg1196@gmail.com>
2019-08-11 01:20:33 -0500

    Show the four graphs

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+"""Gui for show Sin and Cos."""
 import tkinter as tk
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -20,12 +21,9 @@ canvas1.create_window(100, 200, window=entry1)
 entry2 = tk.Entry(root)
 canvas1.create_window(100, 220, window=entry2)
 
-entry3 = tk.Entry(root)
-canvas1.create_window(100, 240, window=entry3)
-
 
 def insert_number():
-
+    """."""
     global sin_amp
     global cos_amp
 
@@ -33,13 +31,32 @@ def insert_number():
     cos_amp = float(entry2.get())
 
     time = np.arange(0, 2 * np.pi, 0.1)
-    sin = np.sin(time)
 
     figure1 = Figure(figsize=(5, 5), dpi=100)
-    subplot1 = figure1.add_subplot(111)
+
+    sin = sin_amp * np.sin(time)
+    subplot1 = figure1.add_subplot(121)
     subplot1.plot(time, sin)
+
+    cos = cos_amp * np.cos(time)
+    subplot2 = figure1.add_subplot(122)
+    subplot2.plot(time, cos)
+
     plot = FigureCanvasTkAgg(figure1, root)
     plot.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=0)
+
+    figure2 = Figure(figsize=(5, 5), dpi=100)
+
+    add = sin + cos
+    subplot3 = figure2.add_subplot(121)
+    subplot3.plot(time, add)
+
+    multiplication = sin * cos
+    subplot4 = figure2.add_subplot(122)
+    subplot4.plot(time, multiplication)
+
+    plot2 = FigureCanvasTkAgg(figure2, root)
+    plot2.get_tk_widget().pack()
 
 
 button2 = tk.Button(root, text='Click to Draw Chart ', command=insert_number)
