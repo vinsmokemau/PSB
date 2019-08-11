8931e439e89db7abd449dd0478a3d6fd32bf9cae
Mau Munguia <maumg1196@gmail.com>
2019-08-11 00:21:00 -0500

    Erase the second chart

--- gui.py
+++ gui.py
@@ -22,35 +22,18 @@ canvas1.create_window(100, 240, window=entry3)
 
 
 def insert_number():
-    global x1
-    global x2
-    global x3
-    x1 = float(entry1.get())
-    x2 = float(entry2.get())
-    x3 = float(entry3.get())
+
+    global sin_amp
+    global cos_amp
+
+    sin_amp = float(entry1.get())
+    cos_amp = float(entry2.get())
 
     figure1 = Figure(figsize=(5, 5), dpi=100)
     subplot1 = figure1.add_subplot(111)
     subplot1.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
     plot = FigureCanvasTkAgg(figure1, root)
     plot.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=0)
-
-    figure2 = Figure(figsize=(1, 1), dpi=100)
-    subplot2 = figure2.add_subplot(111)
-    labels2 = 'Label1', 'Label2', 'Label3'
-    pie_sizes = [float(x1), float(x2), float(x3)]
-    explode2 = (0, 0.1, 0)
-    subplot2.pie(
-        pie_sizes,
-        explode=explode2,
-        labels=labels2,
-        autopct='%1.1f%%',
-        shadow=True,
-        startangle=90,
-    )
-    subplot2.axis('equal')
-    pie2 = FigureCanvasTkAgg(figure2, root)
-    pie2.get_tk_widget().pack()
 
 
 button2 = tk.Button(root, text='Click to Draw Chart ', command=insert_number)
